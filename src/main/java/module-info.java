eaed840d87ba1305ee6d7ed69e0d62899c1aa9fa
Alexandra Purcarea <apurcarea@ymail.com>
2020-04-16 10:20:07 +0300

    added bouncing ball program

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module EpiSim {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens org.epi to javafx.fxml;
     exports org.epi;
--- src/main/java/org/epi/simulation/Person.java
+++
@@ -1,6 +0,0 @@
-package org.epi.simulation;
-
-public class Person {
-    private final
-
-}
